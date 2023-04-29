02c895c436f595f21c3a4556146be62935d8c9e2
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-29 21:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius = document.getElementById('radius');
+	const volume = document.getElementById('volume');
+	volume.value = Math.floor(4/3*3.13*radius.value*radius.value*radius.value);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
